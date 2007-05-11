0e0c5a0a004caa48a9b8a2678961efc8e5bc418a
top <top@esqueleto.(none)>
2007-05-11 01:37:39 -0300

    removendo parênteses desnecessários na parse tree

--- src/teste-yacc.lisp
+++ src/teste-yacc.lisp
@@ -8,7 +8,7 @@
 
 (deflexer string-lexer
   ("(c|d|e|f|g|a|b)(is|es|isis|eses)?" (return (values 'NOTE %0)))
-  ("(128|16|32|64|1|2|4|8)" (return (values' DUR %0)))
+  ("(128|16|32|64|1|2|4|8)" (return (values 'DUR %0)))
   ("(\-|\_|\\^)." (return (values 'ARTICULATION %0)))
   ("[:space:]+" ); (return (values 'WHITESPACE %0)))
   ("-+\n")
@@ -19,9 +19,22 @@
   ("\\}" (return (values '|}| '|}|)))
   )
 
-(defun print-string (a b)
-  (declare (ignore a))
-  (print b))
+(defun flatten-list (l)
+  (when l
+    (if (atom l)
+        l
+        (if (atom (car l))
+            (cons (car l) (flatten-list (cdr l)))
+            (nconc (flatten-list (car l)) (flatten-list (cdr l)))))))
+
+
+
+(defun flatten (&rest lists)
+  (princ "saida: ")
+  (print lists)
+  ;(reduce #'nconc (mapcar #'flatten-list lists)))
+  (flatten-list lists))
+
 
 (define-parser *expression-parser*
   (:start-symbol music-block)
@@ -44,7 +57,7 @@
 
   (notes
    note-expr
-   (notes note-expr))
+   (notes note-expr #'flatten))
   
   (note-expr
    NOTE
@@ -54,7 +67,7 @@
 
   (articulation-expr
    ARTICULATION
-   (articulation-expr ARTICULATION))
+   (articulation-expr ARTICULATION #'flatten))
   )
 
 (parse-with-lexer (string-lexer "{c d e f}") *expression-parser*)
