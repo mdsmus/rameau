6b50cce56373bcceecd4921b7953a2353de3d11d
top <top@esqueleto.(none)>
2007-07-23 14:10:58 -0300

    mais trabalho na segmentação

--- src/ly-parser-teste.lisp
+++ src/ly-parser-teste.lisp
@@ -184,7 +184,77 @@
           (make-evento :PITCH 55 :OCTAVE 9 :DUR 1/4 :INICIO 1/2)
           (make-evento :PITCH 28 :OCTAVE 9 :DUR 1/4 :INICIO 1/2)
           (make-evento :PITCH 0 :OCTAVE 9 :DUR 1/4 :INICIO 1/2))))
-  )
+
+  (assert-equalp
+    (multiple-value-bind
+          (a b)
+        (normaliza-notas
+         (list
+          (make-evento :pitch 1
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 2
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 3
+                       :octave 1
+                       :inicio 0
+                       :dur 5)
+          (make-evento :pitch 4
+                       :octave 1
+                       :inicio 0
+                       :dur 4)))
+      a)
+    (list
+     (make-evento :pitch 1
+                  :octave 1
+                  :inicio 0
+                  :dur 2)
+     (make-evento :pitch 2
+                  :octave 1
+                  :inicio 0
+                  :dur 2)
+     (make-evento :pitch 3
+                  :octave 1
+                  :inicio 0
+                  :dur 2)
+     (make-evento :pitch 4
+                  :octave 1
+                  :inicio 0
+                  :dur 2)))
+
+  (assert-equalp
+    (multiple-value-bind
+          (a b)
+        (normaliza-notas
+         (list
+          (make-evento :pitch 1
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 2
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 3
+                       :octave 1
+                       :inicio 0
+                       :dur 5)
+          (make-evento :pitch 4
+                       :octave 1
+                       :inicio 0
+                       :dur 4)))
+      b)
+    (list
+     (make-evento :pitch 4
+                  :octave 1
+                  :inicio 2
+                  :dur 2)
+     (make-evento :pitch 3
+                  :octave 1
+                  :inicio 2
+                  :dur 3))))
 
 (run-tests)
-
--- src/pardo.lisp
+++ src/pardo.lisp
@@ -14,8 +14,8 @@
   
 
 
-(defun segmentos-minimos (musica)
-  "Extrai os segmentos mínimos de uma música já no formato interno"
+(defun agrupa-inicio (musica)
+  "Separa a musica em pedaços com inicio igual, primeira etapa da segmentação"
   (when musica
     (let* ((primeiro (first musica))
            (inicio (evento-inicio primeiro))
@@ -25,4 +25,65 @@
         (cons (cons primeiro grupo)
               (segmentos-minimos restante))))))
 
+(defun normaliza-notas (segmento)
+  (let ((sobras nil)
+        (tamanho (evento-dur (first segmento))))
+    (values (mapcar (lambda (nota)
+                      (if (= (evento-dur nota) tamanho)
+                          nota
+                          (progn
+                            (push (make-evento
+                                   :pitch (evento-pitch nota)
+                                   :octave (evento-octave nota)
+                                   :dur (- (evento-dur nota) tamanho)
+                                   :inicio (+ tamanho (evento-inicio nota)))
+                                  sobras)
+                            (make-evento
+                             :pitch (evento-pitch nota)
+                             :octave(evento-octave nota)
+                             :dur tamanho
+                             :inicio (evento-inicio nota)))))
+                    segmento)
+            sobras)))
 
+
+(let ((x (list
+          (make-evento :pitch 1
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 2
+                       :octave 1
+                       :inicio 0
+                       :dur 2)
+          (make-evento :pitch 3
+                       :octave 1
+                       :inicio 0
+                       :dur 5)
+          (make-evento :pitch 4
+                       :octave 1
+                       :inicio 0
+                       :dur 4))))
+  (normaliza-notas x))
+
+
+
+(defun segmentos-minimos (musica)
+  "Pega uma musica agrupada em pedaços de inicio igual e cria os
+   segmentos minimos.  Para isso é preciso garantir que todos os
+   pedaços tem o mesmo início e o mesmo fim. Escolhe-se o menor
+   fim de cada pedaço e, caso haja alguma nota com fim posterior,
+   divide-se ela e coloca-se o resto no próximo pedaço."
+  (when musica
+    (let ((ordenados (mapcar
+                      (lambda (x) (sort x (lambda (x y)
+                                            (< (evento-dur x)
+                                               (evento-dur y)))))
+                      musica)))
+      (multiple-value-bind
+            (segmento sobras)
+          (normaliza-notas (first ordenados))
+        (cons segmento (cons (append sobras
+                                     (second ordenados))
+                             (cddr ordenados)))))))
+        
