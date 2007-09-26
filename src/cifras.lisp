5929017b4fd5eaada5e5a30c0e56554ff0531a4b
Pedro Kroger <pedro.kroger@gmail.com>
2007-09-26 19:58:31 -0300

    codigo basico para converter cifras para acorde e vice-versa

--- src/cifras.lisp
+++ src/cifras.lisp
@@ -1,21 +1,9 @@
 (in-package #:rameau)
 
-;; (cifra->acorde 'Cm7.9b.13/Eb)
-;; (cifra->acorde 'C#m7/G#)
-;; (parse-acrescimos "7.9b")
-
 (defun parse-acrescimos (cifra)
   ;; por enquanto só funciona com acordes simples, como Cm7
   (when cifra
     (parse-integer (first (cl-ppcre:split "\\." cifra)))))
-
-(defun %codigo-nota (nota)
-  (note->code (string-downcase nota)))
-
-(defun latin->lily (nota)
-  "Aceita uma string com o nome da nota em latin e retorna a
-representação do lilypond. Exemplo: (latin->lily \"Eb\") => \"ees\""
-  (print-note (code->note (%codigo-nota nota)) 'lily))
 
 (defun get-modo (abrev)
   (case (string->symbol abrev)
@@ -24,40 +12,75 @@ representação do lilypond. Exemplo: (latin->lily \"Eb\") => \"ees\""
     (+ 'aug)
     (t 'maj)))
 
-(defun parse-acorde (acorde)
-  (when acorde
-    (string->symbol (latin->lily acorde))))
+(defun parse-fundamental (fundamental)
+  (when fundamental
+    (string->symbol (latin->lily fundamental))))
 
-(defun qual-inversao? (acorde baixo)
-  (print baixo)
-  (let ((inversao (first (interval->code (interval (%codigo-nota baixo)
-                                                   (%codigo-nota acorde))))))
-    (print inversao)
-    (case inversao
-      (1 0)
-      (3 1)
-      (5 2)
-      (7 3)
-      (t (if (null inversao)
-             0
-             inversao)))))
+(defun %chord-interval-code (fundamental baixo)
+  "Retorna uma lista com o intervalo tonal entre o baixo e a
+fundamental do acorde."
+  (interval->code (interval (note->code baixo)
+                            (note->code fundamental))))
+
+(defparameter *inversoes-pop* '((1 0) (3 1) (5 2) (7 3)))
+
+(defun qual-inversao? (fundamental baixo)
+  (if baixo
+      (let ((intervalo (first (%chord-interval-code fundamental baixo))))
+        (if intervalo
+            (second (find intervalo *inversoes-pop* :key #'first))
+            (error "não conheço inversão")))
+      0))
+
+(defun qual-intervalo-no-baixo? (inversao)
+  (first (find inversao *inversoes-pop* :key #'second)))
+
+;; (cifra->acorde 'Cm7.9b.13/Eb)
+;; (cifra->acorde 'C#m7/G#)
 
 (defun cifra->acorde (cifra)
-  (cl-ppcre:register-groups-bind (acorde modo acrescimos baixo)
-      ("([cdefgab]+[#b]?)(m|o)?([0-9.Mb\\+]+)?/?([cdefgab]+[#b]?)"
-       (symbol->string cifra) :sharedp t)
-    (list (parse-acorde acorde)
-          (get-modo modo)
-          (qual-inversao? acorde baixo)
-          (parse-acrescimos acrescimos))))
+  (let ((cifra-list (cl-ppcre:split "/" (symbol->string cifra))))
+    (cl-ppcre:register-groups-bind (fundamental modo acrescimos)
+        ("([cdefgab]+[#b]?)(m|o)?([0-9.Mb\\+]+)?" (first cifra-list) :sharedp t)
+      (remove-if #'null (list (parse-fundamental fundamental)
+                              (get-modo modo)
+                              (parse-acrescimos acrescimos)
+                              (qual-inversao? fundamental (second cifra-list)))))))
 
-(loop for acorde in '(G G C/E (C7/E) D/F G BM/D (BM7/D) EM C C7/B F#O/A G D
-                      G D/F# E7/G F#O/A G/B AM7/C D (D7) G
-                      G G AM G/B G/B G/B D7/A G D
-                      EM EM BM/D AM/C G/B D7 G G/B G7 C
-                      G D/F# (D7/F#) G F#O7/A G/B G D (D7) EM C G D
-                      G D/F# C/E EM (EM7/D) AM7/C D7 (D7/C) G)
-     when (atom acorde)
-     collect acorde)
+;; TODO: lidar com 7m 7M, 5+, etc
+(defun get-intervalo-inversao-pop (modo inversao)
+  (code->interval (list (qual-intervalo-no-baixo? inversao)
+                        (case inversao
+                          (1 modo)
+                          (2 'just)
+                          (3 'min)))))
 
-(cifra->acorde 'G)
+(defun get-inversao-pop (tonica modo inversao)
+  (unless (= inversao 0)
+    (print-note (code->note (+ (note->code tonica)
+                               (get-intervalo-inversao-pop modo inversao)))
+                'latin)))
+  
+(defun acorde->cifra (acorde)
+  (destructuring-bind (tonica modo inversao &optional acrescimos &rest resto) acorde
+    (let ((fundamental (lily->latin  (symbol->string tonica))))
+      (format nil "~a~@[~a~]~@[/~:(~a~)~]"
+              (case modo
+                (maj (format nil "~a" fundamental))
+                (min (format nil "~am" fundamental))
+                (dim (format nil "~ao" fundamental))
+                (aug (format nil "~a+" fundamental)))
+              acrescimos
+              (get-inversao-pop fundamental modo inversao)))))
+
+(defun read-pop-file (file)
+  (read-from-string (format nil "(~a)" (file-string file))))
+
+(defun gera-gabarito-file (file)
+  (with-open-file (f (concat file ".gab") :direction :output :if-exists :supersede)
+    (loop for c in (read-pop-file (concat file ".pop")) do
+       ;; ignora o que está entre parenteses
+         (unless (listp c)
+           (format f "~(~a~)~%" (cifra->acorde c))))))
+
+;;(gera-gabarito-file "/home/kroger/doc/pesquisa/analise-harmonica/literatura/bach-corais/001")
