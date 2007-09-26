2006c132476f9b4e68b72ee26379590f4a7e3f72
Alexandre Passos <alexandre.tp@gmail.com>
2007-09-26 13:23:34 -0300

    pra eu (passos) poder trabalhar no slime sem ele reclamar do asd

---
+++ src/load.lisp
@@ -0,0 +1,18 @@
+(require 'yacc)
+(require 'lexer)
+(require 'cl-fad)
+
+(load "/home/top/programas/analise-harmonica/src/lib/lisp-unit.lisp")
+
+(load "/home/top/programas/analise-harmonica/src/packages.lisp")
+(load "/home/top/programas/analise-harmonica/src/utils.lisp")
+(load "/home/top/programas/analise-harmonica/src/musiclib.lisp")
+(load "/home/top/programas/analise-harmonica/src/formato.lisp")
+(load "/home/top/programas/analise-harmonica/src/parser.lisp")
+(load "/home/top/programas/analise-harmonica/src/parser-grammar.lisp")
+(load "/home/top/programas/analise-harmonica/src/segmento.lisp")
+(load "/home/top/programas/analise-harmonica/src/pardo.lisp")
+(load "/home/top/programas/analise-harmonica/src/test-musiclib.lisp")
+(load "/home/top/programas/analise-harmonica/src/test-parser.lisp")
+
+
