fa72e3b11edeb18b3de5eeddb440033c8fd44f27
Alexandre Passos <alexandre.tp@gmail.com>
2007-09-27 20:08:49 -0300

    testes pra pardo

---
+++ src/test-pardo.lisp
@@ -0,0 +1,23 @@
+
+(in-package :rameau)
+
+(lisp-unit:define-test compara-gabarito-pardo
+  (lisp-unit:assert-true
+   (compara-gabarito-pardo '((c maj) (c maj) (c dim 7))
+                           '((c maj 0) (c maj 1) (c dim 2 7))))
+  (lisp-unit:assert-false
+   (compara-gabarito-pardo '((c maj) (c maj) (c dim 7))
+                           '((c maj 1) (c maj 0 7) (c dim 7)))))
+
+
+(lisp-unit:define-test compara-gabarito-pardo-individual
+  (lisp-unit:assert-true
+   (compara-gabarito-pardo-individual '(c maj 7)
+                                      '(c maj 0 7))
+  (lisp-unit:assert-false
+   (compara-gabarito-pardo-individual '(c maj)
+                                      '(c maj 0 7)))))
+
+
+
+    
