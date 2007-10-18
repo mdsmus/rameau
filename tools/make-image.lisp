cb846ca05e7ba38e88d3786ff1947fa5edaca4da
Pedro Kroger <pedro.kroger@gmail.com>
2007-10-18 07:53:47 -0300

    suporte inicial em make-image.lisp para gerar ecl

--- tools/make-image.lisp
+++ tools/make-image.lisp
@@ -1,5 +1,8 @@
+#+(or sbcl ecl) (require 'asdf)
+
 (defun main-path ()
-  (format nil "~a" (or #+sbcl *default-pathname-defaults*
+  (format nil "~a" (or #+ecl (si::getcwd)
+                       #+sbcl *default-pathname-defaults*
                        #+cmu (first (ext:search-list "default:"))
                        #+clisp (ext:default-directory))))
 
@@ -9,7 +12,10 @@
 #-ecl(load "tools/rameau.lisp")
 #+sbcl(sb-ext:save-lisp-and-die "rameau" :executable t :toplevel #'main)
 #+cmu(extensions:save-lisp "cmurameau" :init-function #'main)
-#+ecl(compile-file "tools/rameau.lisp" :system-p t)
+#+ecl(progn (compile-file "tools/rameau.lisp" :system-p t)
+            ;;(c:build-program "eclrameau" :lisp-files '("rameau.o"))
+            )
+
 #+clisp(ext:saveinitmem "clisprameau" :script t :quiet t :executable t :init-function #'main)
 
 #+(or ecl clisp)(quit)
