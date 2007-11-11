ed1e673629c5c8800086c4076651af21c6ec59fd
Alexandre Passos <alexandre.tp@gmail.com>
2007-11-11 01:55:30 -0300

    Consertando unit tests para comparação do gabarito do algoritmo de pardo

--- src/test-pardo.lisp
+++ src/test-pardo.lisp
@@ -1,24 +1,14 @@
 
 (in-package :rameau)
 
-(lisp-unit:define-test compara-gabarito-pardo
-  (lisp-unit:assert-true
-   (compara-gabarito-pardo '((c maj) (c maj) (c dim 7))
-                           '((c maj 0) (c maj 1) (c dim 2 7))))
-  (lisp-unit:assert-true
-   (compara-gabarito-pardo '((c maj) (c maj) (c dim 7))
-                           '((c maj 0) ((fis maj 0 6 8) (c maj 1)) (c dim 2 7))))
-  (lisp-unit:assert-false
-   (compara-gabarito-pardo '((c maj) (c maj) (c dim 7))
-                           '((c maj 1) (c maj 0 7) (c dim 7)))))
 
 
 (lisp-unit:define-test compara-gabarito-pardo-individual
   (lisp-unit:assert-true
-   (compara-gabarito-pardo-individual '(c maj 7)
+   (compara-gabarito-pardo-individual '(c maj 0 7)
                                       '(c maj 0 7))
   (lisp-unit:assert-false
-   (compara-gabarito-pardo-individual '(c maj)
+   (compara-gabarito-pardo-individual '(c maj 0)
                                       '(c maj 0 7)))))
 
 
