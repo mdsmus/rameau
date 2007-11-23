eb4eed24488fccf8b80aca62043c7630591e0faa
Pedro Kroger <pedro.kroger@gmail.com>
2007-11-23 08:48:10 -0300

    fecha #430

--- src/rameau.asd
+++ src/rameau.asd
@@ -18,6 +18,7 @@
                (:file "pardo")
                (:file "temperley")
                (:file "cifras")
+               (:file "sbcl")
                (:file "kern2lily")
                (:file "test-utils")
                (:file "test-musiclib")
---
+++ src/sbcl.lisp
@@ -0,0 +1,37 @@
+(in-package "SB-PROFILE")
+
+(defun report ()
+  "Report results from profiling. The results are approximately adjusted
+for profiling overhead. The compensation may be rather inaccurate when
+bignums are involved in runtime calculation, as in a very-long-running
+Lisp process."
+  (unless (boundp '*overhead*)
+    (setf *overhead*
+          (compute-overhead)))
+  (let ((time-info-list ())
+        (no-call-name-list ()))
+    (dohash (name pinfo *profiled-fun-name->info*)
+      (unless (eq (fdefinition name)
+                  (profile-info-encapsulation-fun pinfo))
+        (warn "Function ~S has been redefined, so times may be inaccurate.~@
+               PROFILE it again to record calls to the new definition."
+              name))
+      (multiple-value-bind (calls ticks consing profile)
+          (funcall (profile-info-read-stats-fun pinfo))
+        (if (zerop calls)
+            (push name no-call-name-list)
+            (push (make-time-info :name name
+                                  :calls calls
+                                  :seconds (compensate-time calls
+                                                            ticks
+                                                            profile)
+                                  :consing consing)
+                  time-info-list))))
+
+    (setf time-info-list
+          (sort time-info-list
+                #'>=
+                :key #'time-info-seconds))
+    (print-profile-table time-info-list)
+    (values)))
+
