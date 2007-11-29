61ebd3bddbdbed01ce935a36fcb2cbc78c3799d5
Pedro Kroger <pedro.kroger@gmail.com>
2007-11-29 14:47:47 -0800

    corrige geração de setima c--7

--- src/test-cifras.lisp
+++ src/test-cifras.lisp
@@ -3,7 +3,7 @@
 
 (define-test pop2cifra
   (assert-equal '(* 2 (("fis" "dim" 3) ("fis" "dim" 3 "7-"))) (pop2cifra 'f#°/a--7))
-  (assert-equal '(* 2 (("c" "maj" 0) ("c" "maj" 0 "7"))) (pop2cifra 'c--7))
+  (assert-equal '((c "maj" 0) ((c "maj" 0 "7") (m! "bes"))) (pop2cifra 'c--7))
   (assert-equal (format nil "(c maj 0)~%(c maj 3 7)") (pop2cifra 'c__7))
   (assert-equal (format nil "(c maj 0)~%(c maj 0 7)") (pop2cifra 'c==7))
   (assert-equal (format nil "(c maj 0)~%((c maj 3 7) (m! bes))") (pop2cifra 'c_-7)))
