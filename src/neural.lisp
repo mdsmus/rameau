503f346eed8ee2fdecb4ac3e2ea643e4a2d904de
Alexandre Passos <alexandre.tp@gmail.com>
2008-01-02 17:19:24 -0300

    Rede neural com contexto. Código terrível, copiado e colado, pra ser melhorado mais tarde.

--- src/neural.lisp
+++ src/neural.lisp
@@ -118,8 +118,8 @@
 (defun compara-gabarito-simple-net-individual (resultado gabarito)
   (with-system rameau:tempered
     (if (and (chordp resultado) (chordp gabarito))
-        (equal (chord-fundamental resultado)
-               (chord-fundamental gabarito))
+        (equal (note->code (chord-fundamental resultado))
+               (note->code (chord-fundamental gabarito)))
         (equal (type-of resultado) (type-of gabarito)))))
 
 (defun compara-gabarito-simple-net (resultado gabarito)
@@ -130,123 +130,90 @@
 
 (registra-algoritmo "Simplenet" #'aplica-simple-net #'compara-gabarito-simple-net)
 
-    
-#|
+
+(defvar *context-net* nil)
+
+(defparameter *context-net-file* (concat (rameau-path) "context-net.fann"))
+(defparameter *context-net-train-data* (concat (rameau-path) "context-net-train.data"))
 
 
-(eval-when (:compile-toplevel :load-toplevel)
-  (defparameter *simple-netspec* (netspec 12 12 12))
-  (defvar *simple-net* (mk-net *simple-netspec*)))
+(defun load-context-net ()
+  (if (cl-fad:file-exists-p *context-net-file*)
+      (setf *context-net* (load-from-file *context-net-file*))
+      (progn
+        (treina-context-net)
+        (load-context-net))))
 
-(defun prepara-entrada-treinamento (coral)
-  (make-patterns
-   12
-   (loop
-      for seg in coral
-      collect (cria-pattern-segmento seg))))
-
-(defun prepara-saida-treinamento (gabarito)
-  (make-patterns
-   12
-   (loop
-      for nota in gabarito
-      for atual = (lista-de-zeros 12) then (lista-de-zeros 12)
-      when (chordp nota) do
-        (incf (nth (note->code (stringify (chord-fundamental nota))) atual))
-      collect atual)))
-
-(defun simple-net-training-function (entrada saida)
-  (trainer *simple-netspec*
-           *simple-net*
-           entrada
-           saida
-           :method :cg
-           :max-cycles 10))
-
-(defun treina-simple-net (coral gabarito)
-  (with-system rameau:tempered
-    (let ((coral (segmentos-minimos (parse-file coral))))
-      (loop for i from 0 to 11
-         do (simple-net-training-function
-                     (prepara-entrada-treinamento (transpose-segmentos coral i))
-                     (prepara-saida-treinamento (transpose-chords gabarito i)))))))
-
-(defun extrai-resultado-simple-net (simple-net)
-  (let ((res (coerce (layer-act-vec (svref simple-net 2)) 'list)))
-    (loop for i from 0
-       for r in res
-       when (> r *correctness-treshold*)
-         return (make-chord :fundamental (print-note (code->note i) 'latin))
-       finally (return (make-melodic-note)))))
-
-
-(defun aplica-simple-net (segmento)
-  (setf (layer-act-vec (svref *simple-net* 0))
-        (aref (make-patterns 12 (list (cria-pattern-segmento segmento))) 0))
-  (activation-fn *simple-netspec* *simple-net*)
-  (extrai-resultado-simple-net *simple-net*))
-
-(defun gera-gabarito-simple-net (segmentos)
-  (mapcar #'aplica-simple-net segmentos))
-
-
-(registra-algoritmo "Simplenet" #'gera-gabarito-simple-net #'compara-gabarito-simple-net)
-
-;; Rede neural com contexto, dois segmentos antes e um depois
-;;
-
-
-(defparameter *tamanho-contexto* 4)
-
-(eval-when (:compile-toplevel :load-toplevel)
-  (defparameter *context-netspec* (netspec 48 20 12))
-  (defvar *context-net* (mk-net *context-netspec*)))
+(defun save-context-net ()
+  (save-to-file *context-net* *context-net-file*))
 
 (defun cria-pattern-contexto (segmentos)
   (reduce #'nconc segmentos :key #'cria-pattern-segmento))
 
-(defun prepara-entrada-treinamento-contexto (coral)
-  (let ((coral (cons nil (cons nil coral)))
-        (tamanho (length coral)))
-    (make-patterns
-     (* 12 *tamanho-contexto*)
-     (loop for i from 0 to (1- tamanho)
-        for c = coral then (cdr c)
-        collect (cria-pattern-contexto
-                 (safe-retorna-n-elementos c *tamanho-contexto*))))))
-
-(defun activate-context-net ()
-  (activation-fn *context-netspec* *context-net*))
+(defun prepara-exemplos-treinamento-context-net-individual (coral gabarito)
+  (loop for c = (cons nil (cons nil coral)) then (cdr c)
+     for gab in gabarito
+     if (listp gab)
+       nconc (prepara-exemplos-treinamento-simple-net (repeat-list coral (length gab))
+                                                      gab)
+     else
+       nconc (list (list (cria-pattern-contexto (safe-retorna-n-elementos c 4))
+                         (cria-pattern-saida gab)))))
 
 
-(defun aplica-context-net (coral)
-  (setf (layer-act-vec (aref *context-net* 0))
-        (aref (make-patterns (* 12 *tamanho-contexto*)
-                             (list (cria-pattern-contexto
-                                    (safe-retorna-n-elementos coral *tamanho-contexto*))))
-              0))
-  (activation-fn *context-netspec* *context-net*)
-  (extrai-resultado-simple-net *context-net*))
+(defun prepara-exemplos-treinamento-context-net (coral gabarito)
+  (loop for i from 0 to 11
+     for c = (transpose-segmentos coral i)
+     for g = (transpose-chords gabarito i)
+     nconc (prepara-exemplos-treinamento-context-net-individual c g)))
 
-(defun gera-gabarito-context-net (segmentos)
-  (maplist #'aplica-context-net (cons nil (cons nil segmentos)))) 
-
-(defun context-net-training-function (entrada saida)
-  (trainer *context-netspec*
-           *context-net*
-           entrada
-           saida
-           :method :cg
-           :max-cycles 10
-           :ext-funcs (:std-activate activate-context-net)))
-
-(defun treina-context-net (coral gabarito)
+(defun gera-dados-treinamento-context-net ()
   (with-system rameau:tempered
-    (let ((coral (segmentos-minimos (parse-file coral))))
-      (loop for i from 0 to 11
-         do (context-net-training-function
-                     (prepara-entrada-treinamento-contexto (transpose-segmentos coral i))
-                     (prepara-saida-treinamento (transpose-chords gabarito i)))))))
+    (loop for i in '("001" "003" "004" "006" "012" "018" "136")
+       for nome = (first (rameau-tools::processa-files "corais" (list i)))
+       for f = (segmentos-minimos (parse-file nome))
+       for g = (rameau-tools::processa-gabarito nome "corais")
+       nconc (prepara-exemplos-treinamento-context-net f g))))
 
-(registra-algoritmo "Contextnet" #'gera-gabarito-context-net #'compara-gabarito-simple-net)
-|#
+(defun gera-arquivo-treinamento-context-net ()
+  (let* ((dados (gera-dados-treinamento-context-net))
+         (tamanho (length dados)))
+    (with-open-file (f *context-net-train-data* :direction :output)
+      (format f "~a ~a ~a~%" tamanho 48 12)
+      (loop for d in dados
+         do
+           (format f "~{~a ~}~%" (first d))
+           (format f "~{~a ~}~%" (second d))))))
+
+(unless (cl-fad:file-exists-p *context-net-train-data*)
+  (gera-arquivo-treinamento-context-net))
+
+(defun treina-context-net ()
+  (if (cl-fad:file-exists-p *context-net-train-data*)
+      (progn
+        (setf *context-net* (make-net 48 12 12))
+        (train-on-file *context-net*
+                       *context-net-train-data*
+                       5000
+                       100
+                       0.08)
+        (save-context-net)
+        (setf *context-net* nil))
+      (progn
+        (gera-arquivo-treinamento-context-net)
+        (treina-context-net))))
+
+
+(unless (cl-fad:file-exists-p *context-net-file*)
+  (treina-context-net))
+
+(defun aplica-context-net (inputs)
+  (unless *context-net*
+    (load-context-net))
+  (maplist (lambda (x) (extrai-resultado-simple-net
+                        (run-net *context-net*
+                                 (cria-pattern-contexto (safe-retorna-n-elementos x 4)))))
+           (cons nil (cons nil inputs))))
+
+(registra-algoritmo "Contextnet" #'aplica-context-net #'compara-gabarito-simple-net)
+    
