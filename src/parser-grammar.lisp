631a93736a6658c401d9e93cef41c76ee673fad5
Alexandre Passos <alexandre.tp@gmail.com>
2008-03-06 08:47:12 -0300

    Colocando anacruzes pra começar no 0

--- src/parser-grammar.lisp
+++ src/parser-grammar.lisp
@@ -134,7 +134,7 @@
   (note-expr
    (NOTE octave-expr dur-expr  #'cria-nota)
    (SKIP dur-expr #'cria-skip)
-   (PARTIAL dur-expr #'cria-skip))
+   (PARTIAL dur-expr #'do-nothing))
 
   (octave-expr
    (#'empty-octave)
