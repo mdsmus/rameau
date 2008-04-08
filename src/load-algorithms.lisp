8b5cda61070e5190aa4f1464df9fe40b4d074674
Alexandre Passos <alexandre.tp@gmail.com>
2008-04-08 13:31:38 -0300

    Traduzindo load-algorithms.lisp.

--- src/load-algorithms.lisp
+++ src/load-algorithms.lisp
@@ -1,13 +1,13 @@
 ;; Carrega os algoritmos dentro de rameau durante a compilação
 (in-package :rameau)
 
-(defun carrega-algoritmos ()
+(defun load-algorithms ()
   (let ((path (concat *rameau-path* "src/algoritmos/")))
     (loop for alg in (cl-fad:list-directory path)
        when (count-subseq "lisp" (pathname-name alg))
        do (aif (compile-file alg)
                (load it)))))
 
-(carrega-algoritmos)
+(load-algorithms)
 
-(do-not-test carrega-algoritmos)
+(do-not-test load-algorithms)
