3c1cb8fb1536fbde1df065f11a8b36926af49dfb
Alexandre Passos <alexandre.tp@gmail.com>
2008-04-08 13:26:51 -0300

    Traduzindo pardo.

--- src/algoritmos/pardo.lisp
+++ src/algoritmos/pardo.lisp
@@ -15,41 +15,41 @@
   )
 
 
-(defstruct nota-pardo
+(defstruct pardo-grade
   (root)
-  (resultado)
+  (result)
   (template)
-  (gabarito)
-  (segmento))
+  (answer)
+  (sonority))
 
 (defun group-and-count (segment)
-  "Agrupa as notas de mesmo pitch e conta quantas ocorrem no segmento"
+  "Agrupa as notas de mesmo pitch e conta quantas ocorrem no sonority"
   (when segment
     (let ((nota (first segment)))
       (cons (list nota (count nota segment))
             (group-and-count (skip nota segment))))))
 
 (defun segment-to-template (segment)
-  "Converte um segmento em um formato pronto para ser comparado com um template."
+  "Converte um sonority em um formato pronto para ser comparado com um template."
   (let* ((segment (pitches segment))
          (segment (sort segment #'<)))
     (group-and-count segment)))
 
-(defun avalia-template (template segmento)
-  "Compara as notas de um template com as de um segmento e gera a nota.
+(defun score-template (template sonority)
+  "Compara as notas de um template com as de um sonority e gera a nota.
    Seguindo o artigo de pardo, a nota S = P - (M + N), onde P é a soma
-   dos pesos das notas existentes no segmento e no template, M é a soma
-   dos pesos das notas existentes no segmento e não no template e n é o
-   número de notas no template não encontradas no segmento. O valor mínimo
-   é 0 - Mmax - Nmax, onde Mmax é a soma dos pesos das notas do segmento e
+   dos pesos das notas existentes no sonority e no template, M é a soma
+   dos pesos das notas existentes no sonority e não no template e n é o
+   número de notas no template não encontradas no sonority. O valor mínimo
+   é 0 - Mmax - Nmax, onde Mmax é a soma dos pesos das notas do sonority e
    Nmax é o número de notas no template. Essa função começa com esse valor
    mínimo e vai aumentando-o a cada evidência positiva."
   (let ((score (- 0
                   (reduce #'+
-                          (mapcar #'second segmento))
+                          (mapcar #'second sonority))
                   (length template)))
         (encontrados 0))
-    (dolist (nota segmento)
+    (dolist (nota sonority)
       (when (member (first nota) template)
         (setf score (+ score
                        (* 2 (second nota))
@@ -57,29 +57,29 @@
         (incf encontrados)))
     (+ score encontrados)))
 
-(defun da-nota-modificada (template segmento nota)
+(defun final-score (template sonority nota)
   (let ((note-symb (string->symbol (print-note nota 'latin))))
-    (make-nota-pardo :root note-symb
+    (make-pardo-grade :root note-symb
                      :template template
-                     :resultado (avalia-template
+                     :result (score-template
                                  (set-transpose
                                   template
                                   (notename->code nota))
-                                 segmento)
-                     :segmento segmento)))
+                                 sonority)
+                     :sonority sonority)))
 
-(defun avalia-segmento-notas (template segmento notas)
-  (loop for nota in notas collect (da-nota-modificada template segmento nota)))
+(defun score-sonority-notes (template sonority notas)
+  (loop for nota in notas collect (final-score template sonority nota)))
 
 (defun root-weight (res)
-  (let* ((root-note (stringify (nota-pardo-root res)))
+  (let* ((root-note (stringify (pardo-grade-root res)))
          (weight (assoc-item (parse-note root-note)
-                             (nota-pardo-segmento res))))
+                             (pardo-grade-sonority res))))
     (or weight 0)))
 
 
 (defun template-prob (nota)
-  (let ((template (rest (nota-pardo-gabarito nota))))
+  (let ((template (rest (pardo-grade-answer nota))))
     (length
      (member-if (lambda (x)
                   (equal (car x) template))
@@ -87,58 +87,58 @@
 
 (defun dim7? (notas)
   (every (lambda (x)
-           (equal "°" (stringify (second (nota-pardo-gabarito x)))))
+           (equal "°" (stringify (second (pardo-grade-answer x)))))
          notas))
 
-(defun dim7-res (segmento proximo)
-  (let* ((proxima-tonica (nota-pardo-root proximo))
+(defun dim7-res (sonority proximo)
+  (let* ((proxima-tonica (pardo-grade-root proximo))
          (cod (module
                (+ (parse-note (stringify proxima-tonica))
                   (code->interval '(7 maj)))))
          (nota (print-note (code->notename cod) 'latin)))
     (or (find-if
          (lambda (x)
-           (equal nota (stringify (nota-pardo-root x))))
-              segmento)
-        (first segmento))))
+           (equal nota (stringify (pardo-grade-root x))))
+              sonority)
+        (first sonority))))
 
-(defun desempata-pardo (segmento resto)
+(defun tie-break (sonority resto)
   (let* ((proximo (first resto))
-         (max-root (mostn #'root-weight segmento)))
+         (max-root (mostn #'root-weight sonority)))
     (cons 
      (if (= (length max-root) 1)
          (car max-root)
          (let ((temp-prob (mostn #'template-prob max-root)))
            (if (= (length temp-prob) 1)
                (car temp-prob)
-               (if (dim7? segmento)
-                   (dim7-res segmento proximo)
+               (if (dim7? sonority)
+                   (dim7-res sonority proximo)
                    (car max-root)))))
      resto)))
 
 
 
-(defun avalia-segmento (template segmento)
-  "Gera as notas de um segmento comparado com todas as transposições de
+(defun score-sonority (template sonority)
+  "Gera as notas de um sonority comparado com todas as transposições de
    um template."
-  (let ((resultados
+  (let ((results
          (mostn (lambda (x)
-                          (nota-pardo-resultado x))
-                        (avalia-segmento-notas (second template)
-                                               segmento
+                          (pardo-grade-result x))
+                        (score-sonority-notes (second template)
+                                               sonority
                                                (mapcar #'code->notename *notas-interessantes-tonal*)))))
-    (dolist (r resultados)
-      (setf (nota-pardo-gabarito r) (cons (stringify (nota-pardo-root r))
+    (dolist (r results)
+      (setf (pardo-grade-answer r) (cons (stringify (pardo-grade-root r))
                                           (first template))))
-    resultados))
+    results))
 
-(defun pardo (segmento &optional (templates *pardo-templates*))
-  (mostn (lambda (x) (nota-pardo-resultado x))
+(defun pardo (sonority &optional (templates *pardo-templates*))
+  (mostn (lambda (x) (pardo-grade-result x))
                  (reduce #'append
                          (mapcar
-                          (lambda (x) (avalia-segmento
+                          (lambda (x) (score-sonority
                                        x
-                                       (segment-to-template segmento)))
+                                       (segment-to-template sonority)))
                           templates))))
 
 (defun augmented-sixth-template? (template)
@@ -148,21 +148,21 @@
         (equal modo "it"))))
 
 (defun pardo->chord (pardo)
-  (let ((pardo (nota-pardo-gabarito pardo)))
+  (let ((pardo (pardo-grade-answer pardo)))
     (if (augmented-sixth-template? pardo)
         (make-augmented-sixth :type (second pardo))
         (make-chord :fundamental (first pardo)
                     :mode (second pardo)
                     :7th (third pardo)))))
 
-(defun gera-gabarito-pardo (segmentos)
+(defun pardo-classify (sonoritys)
   (with-system tempered
     (mapcar #'pardo->chord
-            (reduce #'desempata-pardo (mapcar #'pardo (temperado segmentos))
+            (reduce #'tie-break (mapcar #'pardo (temperado sonoritys))
                     :from-end t :initial-value nil))))
      
 
-(register-algorithm "S-PB" #'gera-gabarito-pardo #'compara-gabarito-tonal)
+(register-algorithm "S-PB" #'pardo-classify #'compara-gabarito-tonal)
 
 (deftemplates *incf-pardo-templates* 
   ((nil nil) (0 28 55))
@@ -180,14 +180,14 @@
   (("fr" nil) (0 28 42 70))
   )
 
-(defun incf-pardo (segmento)
-  (pardo segmento *incf-pardo-templates*))
+(defun incf-pardo (sonority)
+  (pardo sonority *incf-pardo-templates*))
 
-(defun gera-gabarito-incf-pardo (segmentos)
+(defun incf-pardo-classify (sonoritys)
   (coloca-inversoes
-   segmentos
+   sonoritys
    (mapcar #'pardo->chord
-           (reduce #'desempata-pardo (mapcar #'incf-pardo segmentos)
+           (reduce #'tie-break (mapcar #'incf-pardo sonoritys)
                    :from-end t :initial-value nil))))
 
-(register-algorithm "ES-PB" #'gera-gabarito-incf-pardo #'compara-gabarito-tonal)
+(register-algorithm "ES-PB" #'incf-pardo-classify #'compara-gabarito-tonal)
