ea393bd014a4d4fe3088bbfdd4d546ab57f85ea9
Pedro Kroger <pedro.kroger@gmail.com>
2008-04-15 11:46:38 -0300

    read accidentals separatelly

--- tools/read-notes.lisp
+++ tools/read-notes.lisp
@@ -29,8 +29,7 @@
   (register-groups-bind (note acc octave dur)
       ("(a|b|c|d|e|f|g)(is|es)?(['|,]*)?([0-9.\\*]*)?" string)
     (declare (ignore octave dur))
-    (list note (if acc acc ""))))
-
+    (list note acc)))
 
 (defun clean (string)
   (string-trim " "
@@ -49,10 +48,12 @@
        when (consp n) collect n)))
 
 (defun toca-nota (nota-list)
-  (let ((nota (apply #'concatenate 'string nota-list)))
-    (format t "~a " nota)
-    (sb-ext:run-program "/usr/bin/play"
-                        (list (concat "/home/kroger/src/rameau/samples/" nota ".wav")))))
+  (flet ((play (nota)
+           (sb-ext:run-program "/usr/bin/play"
+                               (list (concat "/home/kroger/src/rameau/samples/" nota ".wav")))))
+    (destructuring-bind (nota acc) nota-list
+      (play nota)
+      (when acc (play acc)))))
 
 (defun read-file-as-sexp (file)
   "Read file named \\texttt{file} as a single sexp"
@@ -67,18 +68,20 @@
 
 (defun toca (voz coral &optional (de 0) ate)
   (let ((string"~a/literatura/bach-corais/~a.lyi"))
-    (loop
-       for nota in
-         (subseq (get-notes voz (format nil string *rameau-path* coral))
-                 de
-                 ate)
-       do (toca-nota nota))))
+    (format t "~a~%"
+            (loop
+               for nota in
+                 (subseq (get-notes voz (format nil string *rameau-path* coral))
+                         de
+                         ate)
+               collect nota
+               do (toca-nota nota)))))
   
 (defun main ()
   (if (< (length sb-ext:*posix-argv*) 3)
       (format t "USO: checa-notas <voz> <coral> [de] [ate]")
       (destructuring-bind (prog voz coral &optional de ate) sb-ext:*posix-argv*
-        (declare (ignore prog))
+        (print (list voz coral de ate))
         (toca voz
               coral
               (if de
