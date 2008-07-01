93d9e73fc9984c43821cd6cbf6b1f5131da29630
Pedro Kroger <pedro.kroger@gmail.com>
2008-07-01 18:22:09 -0300

    parse fchords

--- src/chords-functional.lisp
+++ src/chords-functional.lisp
@@ -5,33 +5,64 @@
 
 (defparameter *roman-functions* '("i" "ii" "iii" "iv" "v" "vi" "vii"))
 
+(defparameter *inversions-template*
+  '(((7)   => nil 7)
+    ((6)   => 1)
+    ((6 4) => 2)
+    ((6 5) => 1 7)
+    ((4 3) => 2 7)
+    ((4 2) => 3 7))
+  "The format is ((<inversion 'code'>) => <inversion number> <7 if the
+  chord has a seventh>.")
+
 (defstruct fchord
   root 7th 9th 11th 13th bass inversion mode function center)
 
-(defun parse-fchord (symbol &optional center)
+(defun match-inversion (inversion-list)
+  (nthcdr 2 (assoc (mapcar #'parse-integer (sort inversion-list #'string>))
+		     *inversions-template*
+		     :test #'equalp)))
+
+(defun %parse-fchord (symbol &optional center)
   (let* ((function-string (symbol-name symbol))
 	 (split-secondary (cl-ppcre:split "/" function-string))
 	 (function (first split-secondary))
 	 (center-function (second split-secondary)))
     (cl-ppcre:register-groups-bind (roman-function rest)
 	("(iii|ii|i|iv|v|vi|vii|III|II|I|IV|V|VI|VII)(.*)?" function)
-      (list roman-function (cl-ppcre:split "\\." rest) center-function))
-))
+      (destructuring-bind (&optional inversion 7th)
+	  (match-inversion (cl-ppcre:split "\\." rest))
+	(make-fchord :root nil
+		     :bass nil
+		     :7th 7th
+		     :inversion inversion
+		     :mode (if (upper-case-p (char roman-function 0)) 'major 'minor)
+		     :function (1+ (position roman-function *roman-functions* :test #'equalp))
+		     :center center)))))
 
-(parse-fchord '|V7|)
-(parse-fchord '|V7+|)
-(parse-fchord '|V4+|)
-(parse-fchord '|V6.5|)
-(parse-fchord '|ii6.5|)
-(parse-fchord '|V4.3/V|)
+(defun sublist-of-centers (list)
+  ;; tem um bug quando repete proxima flag imediatamente: (@a foo @a bar)
+  ;; entra em loop recursivo
+  (labels ((next-flag (list)
+	     (iter (for item in (rest list))
+		   (unless (consp item)
+		     (for x = (symbol-name item))
+		     (print x)
+		     (if (equal #\@ (aref x 0))
+			 (return x)))))
+           (pos (list flag)
+	     (let ((pos (position flag list :test #'equalp)))
+	       (aif pos pos 0))))
+    (when list
+      (aif (next-flag list)
+	   (let ((p (pos list it)))
+	     (cons (subseq list 0 p)
+		   (sublist-of-centers (nthcdr p list))))
+	   (list list)))))
 
-;;;    (make-fchord :root nil
-;;; 		:bass nil
-;;; 		:inversion nil
-;;; 		:mode (if (upper-case-p (character fchord)) 'major 'minor)
-;;; 		:function (1+ (position fchord *roman-functions* :test #'equalp))
-;;; 		:center center)
-;;(parse-fchord "I" "c")
-;; bVII, bI
+;; (defun parse-fchord-list (list)
+;;   (let ((center (first list))
+;; 	)
+;; ))
 
-;;(read-file-as-sexp (concat *rameau-path* "answer-sheets/examples/001.fun") :preserve)
+(sublist-of-centers (read-file-as-sexp (concat *rameau-path* "answer-sheets/examples/001.fun") :preserve))
