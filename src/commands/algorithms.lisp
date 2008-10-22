e3f16a1a9fa7a8e67a31881059222c1e65f66cf5
Pedro Kroger <pedro.kroger@gmail.com>
2008-10-22 11:46:13 -0300

    minor formating and abstractions

--- src/commands/algorithms.lisp
+++ src/commands/algorithms.lisp
@@ -8,7 +8,8 @@
 (in-package :rameau-alg-commands)
 
 (defun make-training-data (options)
-  "Make training data using the information in @var{options} for chord labeling algorithms."
+  "Make training data using the information in @var{options} for chord
+labeling algorithms."
   (setf *training-data*
         (iter (for f in (arg :files options))
               (awhen (path-parse-answer-sheet (format nil "~a" f))
@@ -16,18 +17,24 @@
                                it))))))
 
 (defun make-functional-training-data (options)
-  "Make training data using the information in @var{options} for
-roman numeral functional analysis algorithms."
+  "Make training data using the information in @var{options} for roman
+numeral functional analysis algorithms."
   (setf *training-data*
         (iter (for f in (arg :files options))
               (awhen (path-parse-functional-answer-sheet (format nil "~a" f))
                 (collect (list (sonorities (parse-file f))
                                it))))))
 
+(defun %load-algs (options &key type)
+  (mapcar #'load-alg (filter-algorithms (arg :algorithms options)
+                                        (case type
+                                          (:func *functional-algorithms*)
+                                          (t *algorithms*)))))
+
 (defun algorithms (options)
   "Gives the command-line options in options to the algorithms."
   (make-training-data options)
-  (setf (arg :algorithms options) (mapcar #'load-alg (filter-algorithms (arg :algorithms options) *algorithms*))
+  (setf (arg :algorithms options) (%load-algs options)
         (arg :options options) (process-option-list (arg :options options)))
   (iter (for alg in (arg :algorithms options))
         (do-options alg options)
@@ -36,13 +43,15 @@ roman numeral functional analysis algorithms."
         *training-data* nil))
 
 (register-command :name "algorithms"
-                  :documentation   "Give the specified chord-labeling algorithms the options passed with -o. Used for training."
+                  :documentation "Give the specified chord-labeling
+                  algorithms the options passed with -o. Used for
+                  training."
                   :action #'algorithms)
 
 (defun funalg (options)
   "Give the command-line options to the functional algorithms."
   (make-functional-training-data options)
-  (setf (arg :algorithms options) (mapcar #'load-alg (filter-algorithms (arg :algorithms options) *functional-algorithms*))
+  (setf (arg :algorithms options) (%load-algs options :type :func)
         (arg :options options) (process-option-list (arg :options options)))
   (iter (for alg in (arg :algorithms options))
         (format t "Options for ~a~%" (alg-name alg))
@@ -52,5 +61,7 @@ roman numeral functional analysis algorithms."
         *training-data* nil))
 
 (register-command :name "funalg"
-                  :documentation "Give the specified roman numeral functional analysis algorithms the options passed with -o. Used for training."
+                  :documentation "Give the specified roman numeral
+                  functional analysis algorithms the options passed
+                  with -o. Used for training."
                   :action #'funalg)
